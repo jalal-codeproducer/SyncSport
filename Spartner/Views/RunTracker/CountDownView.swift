338de8403dc225dc6f4b8db762743896ee698d82
alamer-liebrecht-haas <jalal.codeproducer@gmail.com>
2025-03-09 23:16:45 +0100

    add count down view

--- Spartner/Views/RunTracker/CountDownView.swift
+++ Spartner/Views/RunTracker/CountDownView.swift
@@ -5,26 +5,41 @@
 //  Created by Mohammed Jalal Alamer on 09.03.25.
 //
 
+import AVFAudio
 import SwiftUI
 
 struct CountDownView: View {
     @EnvironmentObject var viewModel: RunTrackerViewModel
     @State var timer: Timer?
-    @State var countDown = 3
+    @State var countDown = 4
+    @State var audioPlayer: AVAudioPlayer?
 
     var body: some View {
-        Text("\(countDown)")
-            .font(.system(size: 128))
-            .fontWeight(.semibold)
-            .fontDesign(.rounded)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(.blue)
-            .onAppear {
-                setupCountDown()
+        VStack {
+            if(countDown > 1){
+                Text("\(countDown - 1)")
+                    .font(.system(size: 128))
+                    .fontWeight(.semibold)
+                    .fontDesign(.rounded)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(.blue)
+            } else {
+                Text("GO!")
+                    .font(.system(size: 128))
+                    .fontWeight(.semibold)
+                    .fontDesign(.rounded)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(.blue)
             }
+
+
+        }.onAppear {
+            setupCountDown()
+        }
     }
 
     func setupCountDown() {
+        playBeepSound()
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {
             _ in
             Task { @MainActor in
@@ -36,6 +51,19 @@ struct CountDownView: View {
                     countDown -= 1
                 }
             }
+        }
+    }
+
+    func playBeepSound() {
+        guard let path = Bundle.main.path(forResource: "beep", ofType: "mp3")
+        else { return }
+
+        do {
+            audioPlayer = try AVAudioPlayer(
+                contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print("Error playing sound: \(error.localizedDescription)")
         }
     }
 }
